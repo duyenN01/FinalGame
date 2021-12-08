1bdcea62389cd9209006e6d927342441a9518ae2
duyenN01 <duyen.t.nguyen01@sjsu.edu>
2021-12-07 16:46:49 -0800

    more codes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,46 @@
+'use strict';
+let osc;
+let waveFormSelect;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  osc = new p5.Oscillator('square');
+
+  //a dropdown menu to change osc type
+createP('Select waveform: ');
+waveFormSelect = createSelect();
+  waveFormSelect.position(10,10);
+  waveFormSelect.option('sine');
+  waveFormSelect.option('sawtooth');
+  waveFormSelect.option('square');
+  waveFormSelect.option('triangle');
+waveFormSelect.changed(setWaveForm);
+
+  //osc.setType('');
+
+
+  //osc.start();
+
 }
 
 function draw() {
-  background(220);
+  osc.freq(map(mouseX, 0, width, 60, 1600));
+
+  osc.amp(map(mouseY,0, height, 0, .2 ));
+
+
 }
+
+function setWaveForm(){
+  osc.setType(waveFormSelect.value);
+}
+
+function mousePressed (){
+  osc.start();
+
+}
+
+// fucntion mouseReleased() {
+//
+//
+// }
